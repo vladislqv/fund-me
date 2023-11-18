c0e2c73747a4d57e835bf7b644a9d97d4a742d41
ycho-eth <ychoeth@proton.me>
2023-11-18 18:43:36 +0200

    commit

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,18 +1,18 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {FundMe} from "../src/FundMe.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external returns(FundMe) {
-        HelperConfig helperConfig = new HelperConfig();
-        address ethUsdPriceFeed =  helperConfig.activeNetworkConfig();
+    function run() external returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
-        return fundMe;
+        return (fundMe, helperConfig);
     }
 }
